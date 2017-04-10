f9482a2961fbd29d94a1d6177989f20efed7c591
Sidstumple <cydstumpel@MacBook-Pro-van-Cyd.local>
2017-04-10 23:39:13 +0200

    Working version with styles :art:

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
-const express = require('express');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
-const app = express();
+var users = [];
+var connections = [];
 
 app.set('view engine', 'ejs'); // Render all html via ejs
+app.use('/static', express.static('./static'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res){
   res.render('index');
 });
 
-app.listen(3004, function () {
-  console.log('Example app listening on port 3004!');
+io.on('connection', function(socket){
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
+
+  // Disconnect
+  socket.on('disconnect', function(data){
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected: %s sockets connected', connections.length)
+  });
+
+  // New user
+  socket.on('new user', function(data, callback){
+    callback(true);
+    socket.username = data;
+    users.push(data);
+    updateUsernames();
+  })
+  function updateUsernames(){
+    io.sockets.emit('get users', users)
+  }
+
+  // Send message
+  socket.on('send message', function(message){
+    console.log(message);
+    io.sockets.emit('new message', {msg: message, user: socket.username})
+  });
+
 });
+
+server.listen(process.env.PORT || 3004);
+console.log('listening on *:3004');
