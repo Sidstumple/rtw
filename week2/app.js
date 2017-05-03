33ce24f20018badd864e47e1146c52095a7e5545
Sidstumple <cydstumpel@MacBook-Pro-van-Cyd.local>
2017-05-03 21:42:26 +0200

    it worksssss :tada:

--- week2/app.js
+++ week2/app.js
@@ -1,62 +1,52 @@
 // var express = require('express'); // Express web server framework
-var request = require('request'); // "Request" library
+var request = require('request'); // 'Request' library
 var querystring = require('querystring');
 var env = require('dotenv').config();
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var PouchDB = require('pouchdb');
-
-// var db = new PouchDB('users');
-//
-// // db.put({
-// //   _id: 'dave@gmail.com',
-// //   name: 'David',
-// //   age: 69
-// // });
-//
-// db.changes().on('change', function() {
-//   console.log('Ch-Ch-Changes');
-// });
-//
-// db.replicate.to('http://example.com/mydb');
 
 server.listen(process.env.PORT || 3000);
 console.log('server listening on port 3000');
 
 app.use('/static', express.static('./static'));
 
+var userData = {};
+console.log(userData);
+
+function makeid() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for( var i=0; i < 5; i++ )
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
+
 var client_id = process.env.CLIENT_ID; // Your client id
 var client_secret = process.env.CLIENT_SECRET; // Your secret
 var redirect_uri = process.env.REDIRECT_URI; // Your redirect uri
-var users = [];
 var connections = [];
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  if(app.accessToken){
-    res.redirect('/whatsplaying');
-  } else {
     res.render('index');
-  }
 })
 
 app.get('/login', function(req, res) {
+  id = makeid();
+  userData[id] = {};
+
+  console.log("[NEW USER]");
+
   //It's necessary to encode the permissions string to allow for scopes like channels:read
   var permissions = encodeURIComponent('client');
 
-  console.log("serving Spotify button with permissions: " + permissions);
-
-  // Here we need app to access Express instance called 'app' we created
-  if (app.accessToken){
-    console.log("already have oauth, reroute client to /whatsplaying");
-    res.redirect('/whatsplaying');
-  } else {
-    // your application requests authorization
-    console.log("new instance");
+  console.log('serving Spotify button with permissions: ' + permissions);
+    console.log('new instance');
     var scope = 'user-read-private user-read-email user-read-currently-playing user-read-playback-state user-read-recently-played user-modify-playback-state';
 
     res.redirect('https://accounts.spotify.com/authorize?' +
@@ -64,10 +54,9 @@ app.get('/login', function(req, res) {
         response_type: 'code',
         client_id: client_id,
         scope: scope,
-        redirect_uri: redirect_uri,
-    }));
-  }
-});
+        redirect_uri: redirect_uri
+      }));
+    });
 
 app.get('/callback', function(req, res) {
   var code = req.query.code;
@@ -89,70 +78,109 @@ app.get('/callback', function(req, res) {
   request.post(authOptions, function(error, response, body) {
     var access_token = body.access_token;
     var refresh_token = body.refresh_token;
-    app.accessToken = body.access_token;
-    // console.log(body);
+    console.log(access_token);
+
+    userData[id].accessToken = access_token;
+    console.log("[NEW ACCESSTOKEN] added");
+    console.log(userData);
+
     res.redirect('/whatsplaying');
   });
 });
 
 app.get('/whatsplaying', function(req, res) {
-  // console.log(app.accessToken);
 
-  if (app.accessToken){
+  if (userData[id].accessToken){
+    console.log("///////////[CHECK if this is done]");
 
-    var profile = {
-      url: 'https://api.spotify.com/v1/me/',
-      headers: { 'Authorization': 'Bearer ' + app.accessToken },
-      json: true
-    }
-    request.get(profile, function(error, response, profile) {
-      // console.log(profile);
-      profile.images.map(function (obj) {
-        console.log(obj.url);
-        console.log(profile.display_name);
-        var image = obj.url;
-        res.render('whatsplaying', {profile: profile, proImage: image});
-      })
-    });
+      var profile = {
+        url: 'https://api.spotify.com/v1/me/',
+        headers: { 'Authorization': 'Bearer ' + userData[id].accessToken },
+        json: true
+      }
+
+      request.get(profile, function(error, response, profile) {
+        console.log('hoi');
+        console.log(profile);
+        userData[id].name = profile.display_name;
+        userData[id]._id = profile.id;
+
+        if (profile.images !== '') {
+          profile.images.map(function (obj) {
+            console.log('///////////////[NEW USER DATA]');
+            userData[id].profileImage = obj.url;
+          })
+        }
+        res.render('whatsplaying', {proImage: userData[id].profileImage, profile: profile})
+        })
   } else {
     console.log('no access token for the selected scope yet.');
     res.redirect('/');
   }
 })
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
+  if(userData[id].accessToken) {
+    var options = {
+      url: 'https://api.spotify.com/v1/me/player/currently-playing',
+      headers: { 'Authorization': 'Bearer ' + userData[id].accessToken },
+      json: true
+    }
+    request.get(options, function(error, response, body) {
+      userData[id].albumImage = body.item.album.images[2].url;
+      userData[id].song = body.item.name;
+      userData[id].artist = '';
+
+      body.item.artists.map(function (obj) {
+        userData[id].artist += `${obj.name} `;
+      })
+      socket.emit('start', {users: userData});
+    });
+  }
+
   connections.push(socket);
   console.log('Connected: %s sockets connected', connections.length);
-
-
   // Disconnect
   socket.on('disconnect', function(data){
     connections.splice(connections.indexOf(socket), 1);
-    console.log('Disconnected: %s sockets connected', connections.length)
+    console.log('Disconnected: %s sockets connected', connections.length);
   });
 
   // Checks if a new song is played:
   socket.on('next song', function(data) {
     var options = {
       url: 'https://api.spotify.com/v1/me/player/currently-playing',
-      headers: { 'Authorization': 'Bearer ' + app.accessToken },
+      headers: { 'Authorization': 'Bearer ' + userData[id].accessToken },
       json: true
     }
 
     request.get(options, function(error, response, body) {
-      var image = body.item.album.images[2].url;
+      var storedSong = userData[id].song;
+      var storedArtist = userData[id].artist;
+      var newSong = body.item.name;
+      userData[id].newArtist = '';
       body.item.artists.map(function(obj){
-        app.artists = obj.name;
+        userData[id].newArtist += `${obj.name} `;
       })
 
-      if(data.song !== body.item.name || data.artist !== app.artists) {
-        // console.log('update this sonnng!');
-        socket.emit('update song', {body: body, image: image, artist: app.artists});
+      console.log(userData[id].newArtist);
+
+      if(storedArtist !== userData[id].newArtist || storedSong !== newSong) {
+        console.log("[UPDATE] song");
+        userData[id].albumImage = body.item.album.images[2].url;
+        userData[id].song = body.item.name;
+        userData[id].artist = '';
+
+        body.item.artists.map(function (obj) {
+          userData[id].artist += `${obj.name} `;
+        })
+        console.log(userData);
+        socket.emit('update song', {users: userData});
       } else {
-        // console.log('still listening to the same tune?');
+        console.log('same song');
       }
-    });
-    // console.log('Check if a new song is played');
+    console.log('Check if a new song is played');
   })
 
+});
 });
--- week2/static/js/index.js
+++ week2/static/js/index.js
@@ -1,21 +1,45 @@
 (function() {
-  var song = document.getElementById('song');
-  var artist = document.getElementById('artist');
-  var image = document.getElementById('image');
+  var playlist = document.getElementById('playlist')
   var socket = io();
 
-  socket.on('connection', function (data) {
-    
+  socket.on('start', function (data) {
+    // console.log(data.body.item.artists);
+    // console.log(Object.keys(data.users));
+
+    Object.keys(data.users).forEach(function(obj){
+      console.log(data.users[obj]);
+      playlist.innerHTML +=
+      `<div class="info empty" id="${data.users[obj]._id}">
+        <div id="image" class="image"><img src="${data.users[obj].albumImage}" alt=""></div>
+        <div class="text">
+          <p><strong>Artist: </strong> <span id="artist">${data.users[obj].artist}</span></p>
+          <p><strong>Song: </strong> <span id="song">${data.users[obj].song}</span></p>
+        </div>
+      </div>`
+      var info = document.getElementById('info');
+      document.getElementById(data.users[obj]._id).classList.remove('empty');
+    })
   })
 
   setInterval(function(){
-    socket.emit('next song', {song: song.innerHTML, artist: artist.innerHTML})
-  }, 2000);
+    socket.emit('next song')
+  }, 3000);
 
   socket.on('update song', function(data){
-    // console.log(data.body.item.artists);
-      song.innerHTML = data.body.item.name;
-      artist.innerHTML = data.artist;
-      image.innerHTML = `<img src="${data.image}" alt="">`;
+    var song = document.getElementById('song');
+    var artist = document.getElementById('artist');
+    var image = document.getElementById('image');
+    console.log(Object.keys(data.users, "update song"));
+
+    Object.keys(data.users).forEach(function(obj){
+      var info = document.getElementById('info');
+      console.log(data.users[obj]);
+      if(data.users[obj]._id == document.getElementById(data.users[obj]._id).id){
+        image.innerHTML = `<img src="${data.users[obj].albumImage}" alt="">`
+        artist.innerHTML = data.users[obj].artist;
+        song.innerHTML = data.users[obj].song;
+      }
+      document.getElementById(data.users[obj]._id).classList.remove('empty');
+    })
   })
 })();
